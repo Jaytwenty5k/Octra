36475a464524e22a8a89b90dccaf975b26aafc96
Jaytwenty5k <JaydenWoppererYK@outlook.com>
2025-05-10 17:39:18 +0000

    Initial project structure

---
+++ bot/index.js
@@ -0,0 +1,8 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.login('YOUR_BOT_TOKEN');
---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Deploying project..."
+# Hier können später spezifische Deploy-Schritte hinzugefügt werden
